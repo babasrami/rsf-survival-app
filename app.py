cc231b6f0ad3dd48c79911491a79859c17265f6e
Rami BABAS <babas.rami@gmail.com>
2026-01-26 18:19:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 # ---------------------------
 st.set_page_config(
     page_title="RSF Survival Predictor",
-    page_icon="🧬",
+    page_icon="",
     layout="wide",
     initial_sidebar_state="expanded"
 )
